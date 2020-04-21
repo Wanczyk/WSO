f2768929a4b9ed2990745b1ccc035f4db0e6a612
Martin Wanczyk <wanczyk.martin@gmail.com>
2020-04-21 22:00:03 +0200

    Create ugly but working code that connect and download file over ssh

--- Android/app/src/main/java/com/mwanczyk/wso/MainActivity.kt
+++ Android/app/src/main/java/com/mwanczyk/wso/MainActivity.kt
@@ -1,27 +1,53 @@
 package com.mwanczyk.wso
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.os.AsyncTask
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.jcraft.jsch.Channel
 import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.ChannelSftp
 import com.jcraft.jsch.JSch
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("test")
-        SshTask().execute()
+
+        val folder = filesDir
+        val f = File(folder, "WSO")
+        f.mkdir()
+        val folderPath = folder.toString()
+        val btnClick = findViewById<Button>(R.id.btn_connect)
+        btnClick.setOnClickListener {
+            findViewById<TextView>(R.id.output).text = "Wait..."
+            val ipAddress = findViewById<EditText>(R.id.ip).text.toString()
+            val userName = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+            SshTask().execute(userName, password, ipAddress)
+        }
     }
 
-    class SshTask : AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg p0: Void?): String {
-            val output = executeRemoteCommand("pi", "pass", "192.168.0.22")
+    private inner class SshTask : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String): String? {
+            print("test" + params[0])
+            val output = executeRemoteCommand(username=params[0], password=params[1], hostname=params[2])
             print(output)
             return output
+        }
+
+        override fun onPostExecute(result: String?) {
+            val file = File("/data/user/0/com.mwanczyk.wso/files/output.txt").readText(Charsets.UTF_8)
+            findViewById<TextView>(R.id.output).text = file
+            println(file)
         }
     }
 }
@@ -40,21 +66,31 @@ fun executeRemoteCommand(username: String,
     session.setConfig(properties)
 
     session.connect()
-
     // Create SSH Channel.
     val sshChannel = session.openChannel("exec") as ChannelExec
     val outputStream = ByteArrayOutputStream()
     sshChannel.outputStream = outputStream
 
     // Execute command.
-    sshChannel.setCommand("ls")
+    sshChannel.setCommand("mv WSO/.output.txt WSO/.output-old.txt")
+//    sshChannel.setCommand("ls -la")
     sshChannel.connect()
 
     // Sleep needed in order to wait long enough to get result back.
     Thread.sleep(1_000)
     sshChannel.disconnect()
 
-    session.disconnect()
+    val channel: Channel = session.openChannel("sftp")
+    channel.connect();
+    val sftpChannel = channel as ChannelSftp
 
-    return outputStream.toString()
+
+    sftpChannel.get("WSO/.output-old.txt", "/data/user/0/com.mwanczyk.wso/files/output.txt")
+    sftpChannel.exit()
+
+    session.disconnect()
+    println("reading.... " + "/data/user/0/com.mwanczyk.wso/files/output.txt")
+    val file = File("/data/user/0/com.mwanczyk.wso/files/output.txt").readText(Charsets.UTF_8)
+
+    return file
 }
